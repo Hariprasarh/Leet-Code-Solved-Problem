6d0667d9d7eae4402871e4d561f906a4cbf3a297
HARIPRASATH S <hariprasath2005manoj@gmail.com>
2025-04-07 21:54:42 +0530

    Time: 2 ms (98.48%) | Memory: 44.8 MB (98.52%) - LeetSync

---
+++ 979-di-string-match/di-string-match.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n=s.length();
+        int right=0;
+        int left=n;
+        int[]arr=new int[n+1];
+        for(int i=0;i<n;i++)
+        {
+            if(s.charAt(i)=='I')
+            {
+                arr[i]=right++;
+            }
+            else
+            {
+                arr[i]=left--;
+            }
+        }
+        arr[n]=left;
+        return arr;
+        
+    }
+}
