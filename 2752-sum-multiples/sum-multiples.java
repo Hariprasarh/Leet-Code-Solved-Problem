c8cd07ae338951ad7b9b22010b47736fc4cb9418
HARIPRASATH S <hariprasath2005manoj@gmail.com>
2025-04-07 07:17:09 +0530

    Time: 2 ms (95.55%) | Memory: 40.7 MB (83.44%) - LeetSync

--- 2752-sum-multiples/sum-multiples.java
+++ 2752-sum-multiples/sum-multiples.java
@@ -4,9 +4,7 @@ class Solution {
         for(int i=1;i<=n;i++)
         {
             if(i%3==0||i%5==0||i%7==0)
-            {
                 count=count+i;
-            }
         }
         return count;
     }
