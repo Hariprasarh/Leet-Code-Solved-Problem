54fd37c26b78c33c416a93bd783753a9c584b580
HARIPRASATH S <hariprasath2005manoj@gmail.com>
2025-04-10 16:46:28 +0530

    Time: 62 ms (5.07%) | Memory: 45.8 MB (19.94%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int singleNumber(int[] arr) {
+        int n=arr.length;
+        int count=0;
+        for(int i=0;i<n;i++)
+        { 
+            count=0;
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    count++;
+                }
+            }
+            if(count==1)
+            {
+                return arr[i];
+            }
+        }
+        return 0;
+    }
+}
