cb1023ff4778f4fb67aa7bda931556c19862c247
HARIPRASATH S <hariprasath2005manoj@gmail.com>
2025-08-13 21:06:48 +0530

    Time: 20 ms (95.63%) | Memory: 46.9 MB (6.00%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer>set=new HashSet<>();
+        for(int  i:candyType)
+        {
+            set.add(i);
+            if(set.size()==candyType.length/2)
+            {
+                break;
+            }
+        }
+        return set.size();
+    }
+}
