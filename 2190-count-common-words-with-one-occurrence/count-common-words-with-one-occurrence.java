8f5d7630e425dfba0522c3c139f6ca916592672b
HARIPRASATH S <hariprasath2005manoj@gmail.com>
2025-08-28 20:47:43 +0530

    Time: 12 ms (11.73%) | Memory: 44.2 MB (75.63%) - LeetSync

---
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int countWords(String[] words1, String[] words2) {
+        Map<String,Integer>m1=new HashMap<>();
+        Map<String,Integer>m2=new HashMap<>();
+        for(String i:words1)
+        {
+            m1.put(i,m1.getOrDefault(i,0)+1);
+        }
+        for(String i:words2)
+        {
+            m2.put(i,m2.getOrDefault(i,0)+1);
+        }
+        ArrayList<String>l1=new ArrayList<>();
+        for(Map.Entry<String,Integer>entry:m1.entrySet())
+        {
+            if(entry.getValue()==1)
+            {
+                l1.add(entry.getKey());
+            }
+        }
+         for(Map.Entry<String,Integer>entry:m2.entrySet())
+        {
+            if(entry.getValue()==1)
+            {
+                l1.add(entry.getKey());
+            }
+        }
+        Map<String,Integer>m3=new HashMap<>();
+        for(String i:l1)
+        {
+            m3.put(i,m3.getOrDefault(i,0)+1);
+        }
+        ArrayList<String>l2=new ArrayList<>();
+        int count=0;
+         for(Map.Entry<String,Integer>entry:m3.entrySet())
+        {
+            if(entry.getValue()==2)
+            {
+                l2.add(entry.getKey());
+                count++;
+            }
+        }
+        return count;
+    }
+}
